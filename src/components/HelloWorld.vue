1ee9ac98bd41146edfed27d3c88019c2e42dd1c9
Adjectival <alexd.jacks@gmail.com>
2018-03-08 13:54:25 -0800

    :sparkles: adds missing deployed URL, spaces out layout, adds badges, Lato as new font

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,14 +10,14 @@
       <ul class="card-deck">
         <!-- proj #1 -->
         <li class="card p-3">
-        <a href="https://fwt-helper.firebaseapp.com/">
+        <a href="https://how-to-crypto.herokuapp.com/">
           <p>
-            <h4>How To Mine Crypto</h4>
+            <h4>How To Crypto</h4>
             <img class="card-image" src="../../static/proj1.png" />
           </p>
-          <span class="card p-1 bg-badge">Bootstrap, Material Design </span>
-          <span class="card p-1 bg-badge">Google DataStudio </span>
-          <h6>Lightweight tutorial built at high speed with the derring MDBootstrap micro-framework.</h6>
+          <span class="badge p-1 bg-badge">Bootstrap, Material Design </span>
+          <span class="badge p-1 bg-badge">Google DataStudio </span>
+          <h6 class="mt-2">Lightweight tutorial built at high speed with the handy MDBootstrap micro-framework.</h6>
           <h6>Focus on cross browser and responsive design.</h6>
         </a>
         </li>
@@ -28,26 +28,14 @@
             <h4>Fantasy War Tactics Helper</h4>
             <img class="card-image" src="../../static/proj2.png" />
           </p>
-          <span class="card p-1 bg-badge">Angular2</span>
-          <span class="card p-1 bg-badge">pixlr</span>
-          <h6>Helps sort an army of characters into tactical teams.</h6>
+          <span class="badge p-1 bg-badge">Angular2</span>
+          <span class="badge p-1 bg-badge">pixlr</span>
+          <h6 class="mt-2">Helps sort an army of characters into tactical teams.</h6>
           <h6>Dropdowns filter a NoSQL dataset which Angular makes into arrays.</h6>
           <h6>Slicing a flat dataset can work as well as a relational database.</h6>
         </a>
         </li>
-        <!-- proj #3 -->
-        <li class="card p-3">
-        <a href="https://fwt-helper.firebaseapp.com/">
-          <p>
-            <h4>Font Awesome Cards</h4>
-            <img class="card-image" src="../../static/proj3.png" />
-          </p>
-          <span class="card p-1 bg-badge">VueJS</span>
-          <span class="card p-1 bg-badge">Sass</span>
-          <h6>Sandbox for exploring CSS effects applied to the ubiquitous Font Awesome library.</h6>
-          <h6>Displays usable code for the effects you create.</h6>
-        </a>
-        </li>
+  
         <!-- proj #4 -->
         <li class="card p-3">
         <a href="https://colors-crypto.firebaseapp.com/">
@@ -55,9 +43,9 @@
             <h4>Colors of Crypto</h4>
             <img class="card-image" src="../../static/proj4.png" />
           </p>
-          <span class="card p-1 bg-badge">React</span>
-          <span class="card p-1 bg-badge">widgets</span>
-          <h6>Dashboard of the top crypto currencies with market values and capitalization.</h6>
+          <span class="badge p-1 bg-badge">React</span>
+          <span class="badge p-1 bg-badge">widgets</span>
+          <h6 class="mt-2">Dashboard of the top crypto currencies with market values and capitalization.</h6>
           <h6>Live values from script loading a widget. Color branding sometimes based upon existing design.</h6>
         </a>
         </li>
@@ -68,9 +56,9 @@
             <h4>Terraforma</h4>
             <img class="tf-image" src="../../static/proj5.png" />
           </p>
-          <span class="card p-1 bg-badge">Sketch</span>
-          <span class="card p-1 bg-badge">InVision</span>
-          <h6>Group design case study from code bootcamp.</h6>
+          <span class="badge p-1 bg-badge">Sketch</span>
+          <span class="badge p-1 bg-badge">InVision</span>
+          <h6 class="mt-2">Group design case study from code bootcamp.</h6>
           <h6>I created visual assets and written copy for the mockup, as well as initial concept.</h6>
         </a>
         </li>
@@ -78,10 +66,14 @@
     </div>
     <!-- crypto zone -->
     <h2 class="raisiny">Crypto~currency</h2>
-    <div class="card p-1 m-3">
+    <div class="card py-3 m-3">
       <p>Yes, I mine it. Yes, I own it. Yes, I trade it.</p>
       <h5>You can, too!</h5>
-      <p>Click here to learn more.</p>
+      <a href="https://how-to-crypto.herokuapp.com/">
+      <button class="btn btn-lg btn-secondary">
+        <h4>Click here to learn more.</h4>
+      </button>
+      </a>
     </div>
     <!-- social links -->
     <h2 class="bg-info">Links to More</h2>
@@ -98,7 +90,7 @@
         <a href="https://github.com/Adjectival" target="_blank">
         <li class="card m-3 p-3 social-icon">
           <i class="fab fa-github"></i>
-          <h6>sources
+          <h6>code
           </h6>
         </li>
         </a>
@@ -158,6 +150,7 @@ export default {
 <style scoped>
 .hello {
   background: linear-gradient(to top, gainsboro, azure);
+
   padding-top: 4rem;
   height: 100%;
 }
